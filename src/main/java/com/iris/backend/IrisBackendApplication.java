882de1b573acc84460ab5ecabfc80d9f01a90c45
Noah Baumgartner <noah.baumgartner1@students.unibe.ch>
2025-07-27 14:32:19 +0200

    chaptime zu iris

---
+++ src/main/java/com/iris/backend/IrisBackendApplication.java
@@ -0,0 +1,11 @@
+package com.iris.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisBackendApplication { // Name geändert
+    public static void main(String[] args) {
+        SpringApplication.run(IrisBackendApplication.class, args); // Name hier auch anpassen
+    }
+}
